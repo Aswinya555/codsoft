bebabc809346bb02ae90cdb03227ad64bbce6479
Aswinya555 <gurugiri1391@gmail.com>
2024-12-10 07:12:07 +0530

    Update Gradecalculator.java

--- Gradecalculator.java
+++ Gradecalculator.java
@@ -3,22 +3,17 @@ public class Gradecalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // Request the number of subjects
         System.out.print("How many subjects do you want to enter marks for? ");
         int subjectCount = input.nextInt();
         int totalScore = 0;
 
-        // Loop to get marks for each subject
         for (int i = 1; i <= subjectCount; i++) {
             System.out.print("Enter the marks for subject " + i + ": ");
             int marks = input.nextInt();
             totalScore += marks;
         }
 
-        // Calculate the average percentage
         double percentage = (double) totalScore / (subjectCount * 100) * 100;
-
-        // Determine the grade based on percentage
         char finalGrade;
         if (percentage >= 90) {
             finalGrade = 'A';
@@ -31,8 +26,7 @@ public class Gradecalculator {
         } else {
             finalGrade = 'F';
         }
-
-        // Display the results
+        
         System.out.println("-----------------------------");
         System.out.println("Total Marks: " + totalScore);
         System.out.println("Average Percentage: " + percentage + "%");
